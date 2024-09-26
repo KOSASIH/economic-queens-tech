5c5ce53f8c39d052db8032b999a75d215e74fc93
KOSASIH <kosasihg88@gmail.com>
2024-09-26 13:34:20 +0700

    Create constants.ts

---
+++ common/constants.ts
@@ -0,0 +1,17 @@
+export const API_VERSION = 'v1';
+export const API_PREFIX = `/api/${API_VERSION}`;
+
+export const DATABASE_URL = 'mongodb://localhost:27017';
+export const DATABASE_NAME = 'empowher';
+
+export const JWT_SECRET = 'secretkey';
+export const JWT_EXPIRES_IN = '1h';
+
+export const PI_COIN_ADDRESS_LENGTH = 34;
+export const PI_COIN_PRIVATE_KEY_LENGTH = 64;
+
+export const MENTORSHIP_MODEL_PATH = './models/mentorship_model.json';
+export const EDUCATION_MODEL_PATH = './models/education_model.json';
+
+export const DEFAULT_PAGE_SIZE = 10;
+export const MAX_PAGE_SIZE = 100;
