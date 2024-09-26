9c07c029e102dc8363cfb2a9d2e78a02d3ecf63a
KOSASIH <kosasihg88@gmail.com>
2024-09-26 12:45:07 +0700

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('EmpowHerNet API')
+    .setDescription('API for EmpowHerNet')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+
+bootstrap();
